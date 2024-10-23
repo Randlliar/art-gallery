aa5f9d758441784842b9d9889899795e88bb0df6
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-23 22:13:45 +0300

    Added requests to the gallery-card component

--- src/app/shared/components/gallery-card/gallery-card.component.ts
+++ src/app/shared/components/gallery-card/gallery-card.component.ts
@@ -1,20 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {ArtService} from "../../services/art.service";
+import {NgForOf, NgIf, SlicePipe} from "@angular/common";
+import {ArtType} from "../../../types/art";
 
 @Component({
   selector: 'gallery-card',
   standalone: true,
   templateUrl: './gallery-card.component.html',
+  imports: [
+    SlicePipe,
+    NgIf,
+    NgForOf
+  ],
   styleUrl: './gallery-card.component.scss'
 })
 
 export class GalleryCardComponent implements OnInit {
 
+  items: ArtType[] = [];
+  id: number = 0;
   constructor(private artService: ArtService) {
   }
 
   ngOnInit() {
+    this.artService.getArts(1, 3)
+      .subscribe((item: any) => {
+        this.id = item.data.filter((id: any) => id.id);
+        console.log(this.id)
+        this.items = item.data;
 
+      })
   }
 
 }
