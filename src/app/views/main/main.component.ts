cbefe8a68a3c3b5167d83913e967255a66d71f79
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-23 17:50:10 +0300

    Add section other gallery

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {GalleryCardComponent} from "../../shared/components/gallery-card/gallery-card.component";
+import {SmallCardComponent} from "../../shared/components/small-card/small-card.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    GalleryCardComponent
+    GalleryCardComponent,
+    SmallCardComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
