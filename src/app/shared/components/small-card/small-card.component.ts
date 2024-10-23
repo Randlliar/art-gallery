8d24a4d1bc66ee3aff955a94a60341bc5a2fb7db
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-23 22:13:15 +0300

    Added requests to the small-card component

--- src/app/shared/components/small-card/small-card.component.ts
+++ src/app/shared/components/small-card/small-card.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ArtService} from "../../services/art.service";
+import {NgForOf, NgIf, SlicePipe,} from "@angular/common";
 
 @Component({
   selector: 'small-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    SlicePipe,
+  ],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.scss'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  data: any = [];
+  constructor(private artService: ArtService) {
+  }
+
+  ngOnInit() {
+    this.artService.getArts(1, 9)
+      .pipe()
+      .subscribe((data: any) => {
+        console.log(data.data)
+        this.data = data.data;
+      })
+
+  }
 
 }
