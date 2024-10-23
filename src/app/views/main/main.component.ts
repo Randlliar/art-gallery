b9898423dcc8fe7ea1cca3eb8c77839c7621cb40
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-23 15:55:41 +0300

    Create main component, add search and gallery

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {GalleryCardComponent} from "../../shared/components/gallery-card/gallery-card.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    GalleryCardComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
