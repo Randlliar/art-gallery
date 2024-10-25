79941ee015a25784250619de202cb610cc965b5b
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-25 16:49:55 +0300

    add loader component

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {LoaderService} from "@services/loader.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss'
+})
+export class LoaderComponent implements OnInit {
+
+  constructor(private loaderService: LoaderService) { }
+  isShowed: boolean = false;
+  ngOnInit(): void {
+    this.loaderService.isShowed$.subscribe((isShowed: boolean) => {
+      this.isShowed = isShowed;
+    });
+  }
+
+}
