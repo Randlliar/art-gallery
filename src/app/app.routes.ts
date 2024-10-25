36dea869d932118b24530b14b6ad2e9abab9d3f0
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-25 15:02:38 +0300

    add routes for details and favorites components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import {LayoutComponent} from "./shared/layout/layout.component";
 import {MainComponent} from "./views/main/main.component";
+import {DetailsComponent} from "./views/art/details/details.component";
+import {FavoritesComponent} from "./views/art/favorites/favorites.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      {path: '', component: MainComponent}
+      {path: '', component: MainComponent},
+      {path: 'details/:id', component: DetailsComponent},
+      {path: 'favorites', component: FavoritesComponent},
     ]
   }
 ];
