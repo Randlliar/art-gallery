7197043978d428e47b3fc41b0ff7203e620bec79
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-27 16:58:09 +0300

    Edit pagination

--- src/app/views/main/main.component.spec.ts
+++ src/app/views/main/main.component.spec.ts
@@ -1,17 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+let router: jasmine.SpyObj<Router>;
 import { MainComponent } from './main.component';
+import {Router} from "@angular/router";
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     await TestBed.configureTestingModule({
       imports: [MainComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,10 @@ describe('MainComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to detail page on getMore', () => {
+    component.getMore(5);
+    expect(router.navigate).toHaveBeenCalledWith(['/details/5'], { queryParams: null });
   });
 });
