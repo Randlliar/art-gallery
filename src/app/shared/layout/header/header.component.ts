afdbcfcdec8193c6c3f0dfdf72fc0cbd8ee0f94f
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-30 19:16:04 +0300

    Add hiding home page

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import { NavigationEnd, Router, RouterLink} from "@angular/router";
+import {filter, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMenuOpen = false;
+  currentUrl!: string;
+  subscription!: Subscription;
+  constructor(private router: Router,
+              ) {
 
+  }
+  values: string[] = [];
+  ngOnInit() {
+    this.currentUrl = this.router.url;
+    this.subscription = this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentUrl = event.url;
+    })
+  }
+
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
