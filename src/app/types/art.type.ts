53607a3a07cad0cf23dc90d7bdbb5a225ec9b8a8
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-30 14:55:29 +0300

    Add art type

--- src/app/types/art.type.ts
+++ src/app/types/art.type.ts
@@ -1,14 +1,12 @@
 export type ArtType = {
+  id: number;
   image_id: string;
   title: string;
   artist_title: string;
-  is_public_domain: boolean;
-  id: number;
-  pagination?: number;
-  dimensions?: string
-  artist_display?: string
-  isInFavorite?: boolean;
-  place_of_origin?: string;
-  credit_line?: string;
-  department_title?: string;
+  artist_display: string;
+  place_of_origin: string;
+  dimensions: string
+  credit_line: string;
+  department_title: string;
+  is_public_domain: string;
 }
