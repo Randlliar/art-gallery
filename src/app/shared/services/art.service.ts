0d0473599a586de02461255c3ee45ef98d8a320e
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-30 14:59:23 +0300

    Edit types

--- src/app/shared/services/art.service.ts
+++ src/app/shared/services/art.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
-import {ArtType} from "@type/art.type";
 import {ActiveParamsType} from "@type/active-param.type";
+import {ArtType} from "@type/art.type";
+import {ArtsWrapperType} from "@type/arts-wrapper.type";
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +14,23 @@ export class ArtService {
   constructor(private http: HttpClient) { }
 
 
-  getArts(params: ActiveParamsType): Observable<ArtType[]> {
+  getArts(params: ActiveParamsType): Observable<ArtsWrapperType> {
     const queryParams =  `?page=${params.page}`;
-    return this.http.get<ArtType[]>(`${environment.api}${queryParams}`);
+    return this.http.get<ArtsWrapperType>(`${environment.api}${queryParams}`);
   }
-  getSomeArts(ids: string): Observable<ArtType[]> {
-    return this.http.get<ArtType[]>(`${environment.api}?ids=${ids}`);
+  getSomeArts(ids: string): Observable<ArtsWrapperType> {
+    return this.http.get<ArtsWrapperType>(`${environment.api}?ids=${ids}`);
   }
 
-getArt(id: number): Observable<ArtType> {
-    return this.http.get<ArtType>(`${environment.api}/${id}`)
+getArt(id: number): Observable<ArtType > {
+    return this.http.get<ArtType >(`${environment.api}/${id}`)
 }
 
-  getSearchArts(params: ActiveParamsType): Observable<ArtType[]> {
+  getSearchArts(params: ActiveParamsType): Observable<ArtsWrapperType> {
     const isSearchMode = !!params?.query?.length;
     const requestEndpoint = `${isSearchMode ? '/search' : ''}`;
     const queryParams =  `?page=${params.page}${isSearchMode ? `&q=${params.query}` : ''}`;
-    return this.http.get<ArtType[]>(`${environment.api}${requestEndpoint}${queryParams}`);
+    return this.http.get<ArtsWrapperType>(`${environment.api}${requestEndpoint}${queryParams}`);
   }
 
 }
