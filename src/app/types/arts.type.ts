6d7db923e3077ce80606acc3310f069f1dc418f5
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-30 19:11:56 +0300

    Edit ArtsType

---
+++ src/app/types/arts.type.ts
@@ -0,0 +1,15 @@
+export type ArtsType = {
+  image_id: string;
+  title: string;
+  artist_title: string;
+  is_public_domain: boolean;
+  id: number;
+  pagination?: number;
+  dimensions?: string
+  total_pages: number;
+  artist_display?: string
+  isInFavorite?: boolean;
+  place_of_origin?: string;
+  credit_line?: string;
+  department_title?: string;
+}
