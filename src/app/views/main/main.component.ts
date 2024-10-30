7753696def6fd2aa39937fbb7ac8c76e1bd0fc91
Igor Sharkevich <sharkevich.i@inbox.ru>
2024-10-30 14:57:31 +0300

    Add sort

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -8,7 +8,8 @@ import {ActiveParamsType} from "@type/active-param.type";
 import {debounceTime, distinctUntilChanged, tap} from "rxjs";
 import {LoaderService} from "@services/loader.service";
 import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ArtType} from "@type/art.type";
+import {ArtsType} from "@type/arts.type";
+import {ArtsWrapperType} from "@type/arts-wrapper.type";
 
 @Component({
   selector: 'app-main',
@@ -26,17 +27,19 @@ import {ArtType} from "@type/art.type";
   styleUrl: './main.component.scss'
 })
 export class MainComponent implements OnInit {
-  arts: ArtType[] = [];
-  srt: string = '';
-  searchArts: ArtType[] = [];
-  amountOfPages!: number;
+  public arts: ArtsType[] = [];
+  private srt: string = '';
+  public searchArts: ArtsType[] = [];
+  public amountOfPages!: number;
+  public sortingOpen= false;
 
-  activeParams: ActiveParamsType = {page: 1};
+  public activeParams: ActiveParamsType = {page: 1};
+  public sortDirection: 'asc' | 'desc' = 'asc';
+  public searchControl = new FormControl('');
+  private isSearch = signal(false);
 
-  searchControl = new FormControl('');
-  isSearch = signal(false);
 
-  artworks: ArtType[] = [];
+  artworks: ArtsType[] = [];
   isLoading: boolean = false;
 
   constructor(private router: Router,
@@ -50,6 +53,7 @@ export class MainComponent implements OnInit {
     this.processContent();
     this.handleSearchInput();
   }
+
 
   private processContent() {
     this.loaderService.show();
@@ -69,7 +73,8 @@ export class MainComponent implements OnInit {
   private getArts() {
     this.loaderService.show();
     this.artService.getArts(this.activeParams)
-      .subscribe((data: any) => {
+      .subscribe((data: ArtsWrapperType ) => {
+        console.log( data)
         this.amountOfPages = data.pagination.total_pages;
         this.arts = data.data;
         this.loaderService.hide();
@@ -79,15 +84,34 @@ export class MainComponent implements OnInit {
 
   private getSomeArts(ids: string) {
     this.artService.getSomeArts(ids)
-      .subscribe((data: any) => this.arts = data.data)
+      .subscribe((data: ArtsWrapperType) => this.arts = data.data)
+  }
+
+  public sortArts() {
+    this.artService.getArts(this.activeParams)
+      .subscribe((data: ArtsWrapperType) => {
+        console.log(data.data)
+        this.arts = data.data.sort((a: ArtsType, b: ArtsType) => {
+          if (this.sortDirection === 'asc') {
+            if (a.title < b.title) return -1;
+            if (a.title > b.title) return 1;
+            return 0;
+          } else {
+            if (a.title < b.title) return 1;
+            if (a.title > b.title) return -1;
+            return 0;
+          }
+        })
+        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      })
   }
 
   private getSearchArts() {
     this.loaderService.show();
     this.artService.getSearchArts(this.activeParams)
-      .subscribe((data: any) => {
+      .subscribe((data: ArtsWrapperType) => {
         this.searchArts = data.data;
-        this.srt = data.data.flatMap((item: ArtType) => item.id).join();
+        this.srt = data.data.flatMap((item: ArtsType) => item.id).join();
         this.getSomeArts(this.srt)
         this.loaderService.hide();
       })
